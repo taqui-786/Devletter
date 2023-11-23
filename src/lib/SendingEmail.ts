92ca75b40209c1914c6658d173739b61198782b3
taqui-786 <mdtaqui.jhar@gmail.com>
2023-11-23 20:36:14 +0530

    Fixing emal sending error

--- src/lib/SendingEmail.ts
+++ src/lib/SendingEmail.ts
@@ -4,11 +4,10 @@ import {z} from 'zod'
 import { Resend } from 'resend'
 import { EmailSchema } from './EmailValidator'
 import { ThankyouEmail } from '@/app/component/EmailTemplates'
-// import { EmailFormInput } from '@/app/component/InputForm'
+import { renderAsync } from '@react-email/render'
 
 
 type input = z.infer<typeof EmailSchema>
-
 
 export async function addEntry(data: input){
 const result = EmailSchema.safeParse(data)
@@ -26,6 +25,8 @@ type emailFormInput = z.infer<typeof EmailSchema>
 
 const resend = new Resend(process.env.RESEND_API_KEY)
  export async function SendEmail (data: emailFormInput){
+  const emailTemplate = await renderAsync(ThankyouEmail({}))
+
   const result = EmailSchema.safeParse(data)
   if(result.success){
     const {email} = result.data
@@ -34,7 +35,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
         from :"Devletter <onboarding@resend.dev>",
         to: email as string,
         subject:"Taqui from Devletter",
-        react: ThankyouEmail({})
+        react: emailTemplate
 
       })
       return {success:true, data}
